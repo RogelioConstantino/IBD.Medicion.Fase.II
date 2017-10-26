d4aecd450585011f4d18b93b137cc523165153bf
G311396 <G311396@HOR549642C>
2017-10-26 17:34:34 -0500

    Version Estable.

---
+++ Medicion/Class/Log/LogError.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Reflection;
+
+namespace Medicion.Class.LogError
+{
+   
+    public class LogErrorMedicion :PropertiesLogError
+    {
+        private static string m_exePath = string.Empty;
+        
+
+        public  void LogWrite()
+        {
+            String LogError = "LogError.Log";
+            m_exePath = HttpRuntime.AppDomainAppPath + "LOG";
+            if (!File.Exists(m_exePath + "\\" + LogError))
+                File.Create(m_exePath + "\\" + LogError);
+
+            try
+            {
+                using (StreamWriter w = File.AppendText(m_exePath + "\\" + LogError))
+                    AppendLog(logMessage, w, logModule);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private static void AppendLog(string logMessage, TextWriter txtWriter, string logModule)
+        {
+            try
+            {
+                txtWriter.Write("\r\nLog Entry : ");
+                txtWriter.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                txtWriter.WriteLine("  : Module: ", logModule);
+                txtWriter.WriteLine("  :{0}", logMessage);
+                txtWriter.WriteLine("-------------------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
---
+++ Medicion/Class/Log/PropertiesLogError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Medicion.Class
+{
+    public class PropertiesLogError
+    {
+        public string logMessage { get; set; }
+        public string logModule { get; set; }
+    }
+}
